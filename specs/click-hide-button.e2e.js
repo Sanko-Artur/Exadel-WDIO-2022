a6f134ca7859eaf8f5da5545ddb5cc191cb01300
Artur Sanko <sanartyrv@gmail.com>
2022-03-23 17:32:57 +0300

    Reload files for HW-2

--- specs/click-hide-button.e2e.js
+++ specs/click-hide-button.e2e.js
@@ -1,14 +1,43 @@
 // REMOVE THE BELOW CODE BEFORE START THE EXERCISE
-describe('Check app', function () {
-    it('click on hide button', async function () {
-        await browser.url('https://viktor-silakov.github.io/course-sut');
-        await $('#login').setValue('walker@jw.com');
-        await $('#password').setValue('password');
-        await $('button').click();
-        await browser.pause(3000);
-        await browser.execute('alert(arguments[0] + arguments[1])', 'Hello', ' World!');
-        await browser.pause(3000);
-    });
+describe('HW-2 task #2', function () {
+  const url = 'https://viktor-silakov.github.io/course-sut';
+  const inputForLogin = '#login';
+  const login = 'walker@jw.com';
+  const inputForPassword = '#password';
+  const password = 'password';
+  const buttonLogin = 'button';
+  const spinner = '.spinner-border text-light';
+  const methodForDeleteElementWithinDOM =
+    'document.querySelector(arguments[0]).remove()';
+  const arguments0 =
+    '[class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"]';
+  const hiddenButton = '//button[contains(text() , "alert")]';
+
+  before(async function () {
+    await browser.maximizeWindow();
+    await browser.url(url);
+    await $(inputForLogin).setValue(login);
+    await $(inputForPassword).setValue(password);
+    await $(buttonLogin).click();
+    await $(spinner).waitForDisplayed({ timeout: 15000, reverse: true });
+  });
+
+  it('Удалить sticky - элемент при помощи выполнения Javascript в контексте браузера', async function () {
+    await browser.pause(2000);
+    await browser.execute(methodForDeleteElementWithinDOM, arguments0);
+    await browser.pause(2000);
+  });
+
+  it('кликнуть на кнопку используя метод WebdriverIO ', async function () {
+    await browser.pause(500);
+    await $(hiddenButton).click();
+  });
+
+  it('Подтвердить появившийся alert используя browser.acceptAlert() метод', async function () {
+    await browser.pause(500);
+    await browser.acceptAlert();
+    await browser.pause(500);
+  });
 });
 
-
+// npx wdio run ./wdio.conf.js
--- specs/login-layout.e2e.js
+++ specs/login-layout.e2e.js
@@ -1,16 +1,64 @@
 // REMOVE THE BELOW CODE BEFORE START THE EXERCISE
-describe('Check app', function () {
-    it('should login', async function () {
-        await browser.url('https://viktor-silakov.github.io/course-sut');
-        await $('#login').setValue('walker@jw.com');
-        await $('#password').setValue('password');
-        await $('button').click();
-        await browser.pause(15000);
-        const title = await browser.getTitle();
-        if (title !== 'Report portal') {
-            throw new Error('You don`t login into system!!!')
-        }
-    });
+describe('HW-2 task #1', function () {
+  const url = 'https://viktor-silakov.github.io/course-sut';
+  const inputForLogin = '#login';
+  const login = 'walker@jw.com';
+  const inputForPassword = '#password';
+  const password = 'password';
+  const buttonLogin = 'button';
+  const spinner = '.spinner-border text-light';
+  const userLabel = '#user-label';
+  const nicknameOfUser = 'John Walker';
+  const attrOfElement = 'title';
+  const sideBarMenu = '//nav[@id="sidebarMenu"]/*/ul[@id="first-nav-block"]/li';
+  const cssProperty = 'background-color';
+  const color = 'rgba(255,0,0,1)';
+
+  before(async function () {
+    await browser.maximizeWindow();
+    await browser.url(url);
+    await $(inputForLogin).setValue(login);
+    await $(inputForPassword).setValue(password);
+    await $(buttonLogin).click();
+  });
+
+  // it('Открыть url https://viktor-silakov.github.io/course-sut/', async function () {
+  //   await browser.maximizeWindow();
+  //   await browser.url(url);
+  // });
+
+  // it('Войти в приложения используя учетные данные ', async function () {
+  //   await $(inputForLogin).setValue(login);
+  //   await $(inputForPassword).setValue(password);
+  //   await $(buttonLogin).click();
+  // });
+
+  it('проверить исчезновение preloader', async function () {
+    await $(spinner).waitForDisplayed({ reverse: true });
+  });
+
+  // какая из проверок нужна была для пункта: "проверить действительно ли пользователь с таким логином вошел в систему" ?
+  it('проверить действительно ли пользователь с таким логином вошел в систему #1', async function () {
+    const elem = await $(userLabel);
+    await expect(elem).toHaveTextContaining(nicknameOfUser);
+  });
+
+  it('проверить действительно ли пользователь с таким логином вошел в систему #2', async function () {
+    const elem = await $(userLabel);
+    await expect(elem).toHaveAttributeContaining(attrOfElement, login);
+  });
+
+  it('Навести курсор мыши поочередно на каждый элемент (li) первого блока sidebar, убедится что при наведении background-color элемента не красный "rgba(255,0,0,1)"', async function () {
+    const sideBar = await $$(sideBarMenu);
+    for (li of sideBar) {
+      await browser.pause(500);
+      await $(li).moveTo();
+      const styleInfo = await $(li).getCSSProperty(cssProperty);
+      if (styleInfo.value === color) {
+        throw new Error(`Color was changed to red`);
+      }
+    }
+  });
 });
 
-
+// npx wdio run ./wdio.conf.js
